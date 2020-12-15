a2193343c779563de869a0b7825c51747818a53b
masha <moreshko@andrew.cmu.edu>
2020-12-14 22:35:48 -0500

    sequence set takes list of bed files and catches errors

--- ucsc-pynome/sequence_set.py
+++ ucsc-pynome/sequence_set.py
@@ -14,18 +14,25 @@ class SequenceSet():
     CHROM_COL = 0
     START_COL = 1
     END_COL = 2
-    NAME_COL = 3
 
-    def __init__(self, bed_file_name, genome):
+    def __init__(self, bed_file_names, genome):
         self.genome = genome
-        self.__parse_bed_file(bed_file_name)
+        self.coordinates = list()
+        for filename in bed_file_names:
+            try:
+                self.__parse_bed_file(filename)
+            except (OSError, MalformedBedFileError) as e:
+                print(repr(e))
+                print("Unable to parse bed file " + filename)
+            else:
+                print("Successfully parsed bed file " + filename)
 
     def __is_header_line(self, L):
         first_word = L[0]
         return first_word == "browser" or first_word == "track" or first_word == "#"
 
     def __parse_bed_file(self, bed_file_name):
-        self.coordinates = list()
+        curr_file_coordinates = list()
         with open(bed_file_name) as f:
             num_columns = -1
             for line in f:
@@ -36,18 +43,22 @@ class SequenceSet():
                     if num_columns == -1:
                         num_columns = len(L)
                         if num_columns < self.MIN_NUM_COLS:
-                            return MalformedBedFileError("Not enough columns")
+                            raise MalformedBedFileError("Not enough columns")
                     elif len(L) != num_columns:
-                        return MalformedBedFileError("Number of columns is not the same across all lines")
+                        raise MalformedBedFileError("Number of columns is not the same across all lines")
                     if len(L) > self.MIN_NUM_COLS:
                         # there is additional line data
+                        additional_cols = L[self.MIN_NUM_COLS:]
+                        label = " ".join(additional_cols)
                         coord = Sequence(L[self.START_COL], L[self.END_COL], self.genome, 
-                                            L[self.CHROM_COL], L[self.NAME_COL])
-                        self.coordinates.append(coord)
+                                            L[self.CHROM_COL], label)
+                        curr_file_coordinates.append(coord)
                     else:
                         coord = Sequence(L[self.START_COL], L[self.END_COL], 
                                             self.genome, L[self.CHROM_COL])
-                        self.coordinates.append(coord)
+                        curr_file_coordinates.append(coord)
+        # successfully parsed bed file
+        self.coordinates.extend(curr_file_coordinates)
     
     # will overwrite existing file if bed_file_name already exists
     def to_bed(self, bed_file_name):
@@ -67,7 +78,7 @@ class SequenceSet():
     def to_fasta(self, fasta_file_name):
         f = open(fasta_file_name, "w")
         for coord in self.coordinates:
-            seq = coord.get_sequence() # TODO what to do if this fails?
+            seq = coord.string() # TODO what to do if this fails?
             f.write(">")
             if coord.label != None:
                 f.write(coord.label)
@@ -109,37 +120,38 @@ class SequenceSet():
         redirect = ' 2> log.err'
         os.system(liftover_call + redirect)
 
-        lifted_sequence = SequenceSet(outfile_name, target_genome)
+        lifted_sequence = SequenceSet([outfile_name], target_genome)
         return lifted_sequence
             
 # tests
 def test_hg19_file_creation():
-    ss = SequenceSet("test_files/hg19_ex.bed", "hg19")
-    ss.create_fasta("hg19_ex.fasta")
+    ss = SequenceSet(["test_files/hg19_ex.bed"], "hg19")
+    ss.to_fasta("hg19_ex.fasta")
     ss.to_bed("hg19_new.bed")
 
 def hg19_to_hg38():
-    ss = SequenceSet("test_files/hg19_ex.bed", "hg19")
+    ss = SequenceSet(["test_files/hg19_ex.bed"], "hg19")
     lss = ss.liftover("hg38")
 
 def nonexistent_file():
-    ss = SequenceSet("test_files/hg17_ex.bed", "hg19")
+    ss = SequenceSet(["test_files/hg17_ex.bed"], "hg19")
     lss = ss.liftover("hg38")
 
 def nonexistent_src():
-    ss = SequenceSet("test_files/hg19_ex.bed", "jf89")
+    ss = SequenceSet(["test_files/hg19_ex.bed"], "jf89")
     lss = ss.liftover("hg38")
 
 def bad_src_file():
-    ss = SequenceSet("test_files/hg19_bad.bed", "hg19")
+    ss = SequenceSet(["test_files/hg19_bad.bed"], "hg19")
     lss = ss.liftover("hg38")
 
 def wrong_col_count():
-    ss = SequenceSet("test_files/hg19_bad2.bed", "hg19")
+    ss = SequenceSet(["test_files/hg19_bad2.bed"], "hg19")
     lss = ss.liftover("hg38")
 
+#test_hg19_file_creation()
 #hg19_to_hg38()
-wrong_col_count()
+#wrong_col_count()
 
 # print(ss.coordinates[0].genome)
 # print(ss.coordinates[0].chromosome)
