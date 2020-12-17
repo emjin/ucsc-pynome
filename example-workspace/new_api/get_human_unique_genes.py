4245226b9cd6fe20c8422604c1b581bdd553f922
Emma Jin <emjin@users.noreply.github.com>
2020-12-17 00:51:30 -0500

    Changed gitignore liftover paths to reflect new file structure

---
+++ example-workspace/new_api/get_human_unique_genes.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append("../..")
+from ucscpynome import Genome
+from ucscpynome import SequenceSet
+
+hg19_seqs = SequenceSet(["hg19_ex.bed"], "hg19")
+primates = ["panTro6", "ponAbe3", "rheMac10"]
+
+intersecting_seqs = set()
+
+# Get sequences for each primate
+for primate in primates:
+    print("Starting " + primate)
+    primate_gene = SequenceSet.liftover(hg19_seqs, primate)
+
+    # Go through the sequences and check if any of them exactly match one of the human ones
+    for primate_seq in primate_gene.sequences:
+        for human_seq in hg19_seqs.sequences:
+            if human_seq.string is primate_seq.string:
+                intersecting_seqs.add(human_seq)
+
+hg19_seqs.sequences = list(set(hg19_seqs.sequences) - intersecting_seqs)
+hg19_seqs.to_fasta("hg19_unique.fasta")
